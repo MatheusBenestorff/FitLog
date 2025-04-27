4116734624f610f09ba41ca42e3cc23caaee1917
Matheus Benestorff <matheusbenestorff@gmail.com>
2025-04-27 15:55:46 -0300

    estrutura do projeto

---
+++ backend/FitLog-spring-app/src/main/java/com/benestorff/FitLog_spring_app/FitLogSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.benestorff.FitLog_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitLogSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitLogSpringAppApplication.class, args);
+	}
+
+}
---
+++ backend/FitLog-spring-app/src/test/java/com/benestorff/FitLog_spring_app/FitLogSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benestorff.FitLog_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitLogSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
